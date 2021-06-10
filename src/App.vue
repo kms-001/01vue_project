0a36abbcc4b3515e5d59b7fd1a5ece586a89c225
kms <min9.9999u@gmail.com>
2021-06-10 10:52:50 +0900

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
 
+  <banner/>
+
+  <div class="black-bg" v-if="proView==true">
+    <div class="white-bg">
+      <img v-bind:src="product[proNum].image">
+      <div> {{product[proNum].title}} </div>
+      <div> {{product[proNum].price}} </div>
+      <div> {{product[proNum].content}} </div>
+      <button v-on:click="proView=false">닫기</button>
+    </div>
+  </div>
+
   <ul class="view">
     <li v-for="(item,i) in product" v-bind:key="i"> 
-      {{product[i].title}} 
+      <img v-bind:src="product[i].image">
+      <div> {{product[i].title}} <span v-on:click="proView=true;proNum=i">[상세보기]</span></div>
+      <div> {{product[i].price}} </div>
     </li>
   </ul>
-
-  <test> </test>
 
 </template>
 
@@ -18,16 +30,30 @@ export default {
   name: 'App',
   data(){
     return{
+      proNum:0,
+      proView:false,
       product:vdata,
     }
   },
   components:{
-    test:banner
+    banner:banner,
   }
 }
 </script>
 
 <style>
+  html,body {height: 100%;}
   *{padding: 0;margin: 0;}
   li{list-style: none;}
+  img{width: 100%}
+  .view li {margin-bottom: 10px;}
+
+  .black-bg{position:fixed;
+           width: 100%; height: 100%;
+           background:rgba(0,0,0,0.5);
+           top:0; display:flex;
+           justify-content: center;
+           align-items: center;}
+  .white-bg {width: 80%; background: #fff;
+            border-radius: 5px; padding: 20px;}
 </style>
