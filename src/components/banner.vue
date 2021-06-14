6840d64f8d9da912299f178892bd0b9da1f9c128
kms <min9.9999u@gmail.com>
2021-06-14 13:34:15 +0900

    vue

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -9,14 +9,14 @@ export default {
     name:'banner',
     data(){
         return{
-            text:'안녕하세요'
+            text:'안녕하세요 :)'
         }
     }
 }
 </script>
 
 <style>
-    .banner {width: 100%; height: 200px; 
-            background: #ccc; display: flex;
+    .banner {width: 100%; height: 100px; 
+            background: rgb(206, 205, 252); display: flex;
             justify-content: center; align-items: center;}
 </style>
